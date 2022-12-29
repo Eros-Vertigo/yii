0d8cec3961647e9346d7b9c03869610c653b90da
原桐 <yt.vertigo0927@gmail.com>
2022-12-29 17:00:36 +0800

    调整一些东西

--- frontend/modules/paypal/Paypal.php
+++ frontend/modules/paypal/Paypal.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * Paypal Payments 贝宝支付网关
  * User: yuantong
  * Date: 2022/12/28
  * Email: <yt.vertigo0927@gmail.com>
@@ -11,19 +11,62 @@ namespace frontend\modules\paypal;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Middleware;
 use Yii;
 use yii\base\Model;
 
 class Paypal extends Model
 {
+    const APPROVE = 'approve';
+    const PAYPAL_ACCESS_TOKEN = 'key:paypal:access_token';
+    const STATUS = [
+        'CREATED' => 201
+    ];
+
+    /**
+     * @var string client_id
+     * @link https://developer.paypal.com/dashboard/applications/edit/
+     */
     public string $clientId;
+    /**
+     * @var string client_secret
+     */
     public string $clientSecret;
 
+    public array $links;
+    /**
+     * @var mixed 同步回调地址
+     */
+    private $return_url;
+    /**
+     * @var mixed 取消支付地址
+     */
+    private $cancel_url;
+    /**
+     * @var mixed 货币代码
+     */
+    public $currency_code = 'USD';
+    /**
+     * @var mixed 金额
+     */
+    public $amount = '100.00';
+    /**
+     * @var mixed 产品名称
+     */
+    public $product_name = 'shenlan test';
+    /**
+     * @var mixed Paypal Order ID
+     */
+    public $order_id;
+
+    /**
+     * 初始化一些参数
+     */
     public function init()
     {
         $this->clientId = "AQCSuHaPI93q833esvuYkXrKzhYR-fgXcZJu5FUh6yTMMIT20Lk4Q4AoFPw7DifHCZ8Vty-qoJToNvBm";
         $this->clientSecret = "ECac-JxHHlJ-hDH9CBF5XH3qYMCGPcaa2He8IfGNM1opI87Vx00GkCYIa9iJgujpwVa3nvxuTNjjSyV0";
+        $this->return_url = '';
+        $this->cancel_url = '';
     }
 
     /**
@@ -33,8 +76,8 @@ class Paypal extends Model
     public function generateToken()
     {
         $redis = Yii::$app->redis;
-        if ($redis->get('paypal-token')) {
-            return $redis->get('paypal-token');
+        if ($redis->get(self::PAYPAL_ACCESS_TOKEN)) {
+            return $redis->get(self::PAYPAL_ACCESS_TOKEN);
         }
 
         $client = new Client([
@@ -54,8 +97,8 @@ class Paypal extends Model
         }
         $result = json_decode($res->getBody()->getContents(), true);
         // 缓存token
-        $redis->set('paypal-token', $result['access_token']);
-        $redis->expire('paypal-token', $result['expires_in']);
+        $redis->set(self::PAYPAL_ACCESS_TOKEN, $result['access_token']);
+        $redis->expire(self::PAYPAL_ACCESS_TOKEN, $result['expires_in']);
 
         return $result['access_token'];
     }
@@ -63,12 +106,13 @@ class Paypal extends Model
     /**
      * 创建订单
      * @throws GuzzleException
+     * if create order successful, return order_id and approve link. then redirect url to pay
      */
     public function checkoutOrder()
     {
         $client = new Client([
             'base_uri' => 'https://api-m.sandbox.paypal.com',
-            'timeout' => 2.0
+            'timeout' => 5.0
         ]);
         try {
             $res = $client->post('/v2/checkout/orders', [
@@ -77,48 +121,46 @@ class Paypal extends Model
                     'intent' => 'CAPTURE',
                     'purchase_units' => [
                         [
-                            'items' => [
-                                [
-                                    'name' => 'T-Shirt',
-                                    'description' => 'Red XL',
-                                    'quantity' => 1,
-                                    'unit_amount' => [
-                                        'currency_code' => 'USD',
-                                        'value' => '100.00',
-                                    ]
-                                ]
-                            ],
+                            'name' => $this->product_name,
                             'amount' => [
-                                'currency_code' => 'USD',
-                                'value' => '100.00',
-                                'breakdown' => [
-                                    'item_total' => [
-                                        'currency_code' => 'USD',
-                                        'value' => '100.00'
-                                    ]
-                                ]
+                                'currency_code' => $this->currency_code,
+                                'value' => $this->amount,
                             ]
                         ]
                     ],
                     'application_context' => [
-                        'return_url' => 'https://example.com/return',
-                        'cancel_url' => 'https://example.com/cancel',
+                        'return_url' => $this->return_url,
+                        'cancel_url' => $this->cancel_url,
                     ]
                 ]
             ]);
-            if ($res->getStatusCode() != 201) {
+            if ($res->getStatusCode() != self::STATUS['CREATED']) {
                 return false;
             }
             $result = json_decode($res->getBody()->getContents(), true);
-            echo "<pre>";
-            var_dump($result);
-            echo "<pre>";
-            exit;
+            $this->order_id = $result['id'];
+            $this->links = $result['links'];
+            return $this->getHref(self::APPROVE);
         } catch (ClientException $e) {
-            echo "<pre>";
-            var_dump($e->getMessage());
-            echo "<pre>";
-            exit;
+            return $e->getMessage();
         }
     }
+
+    /**
+     * 根据 rel 从 links 获取 href
+     * @param $rel mixed restful response link collection
+     * @return mixed|null
+     */
+    public function getHref($rel)
+    {
+        if (is_array($this->links)) {
+            foreach ($this->links as $link) {
+                if ($link['rel'] == $rel) {
+                    return $link['href'];
+                }
+            }
+        }
+        return null;
+    }
+
 }
--- frontend/modules/paypal/controllers/DefaultController.php
+++ frontend/modules/paypal/controllers/DefaultController.php
@@ -3,7 +3,10 @@
 namespace frontend\modules\paypal\controllers;
 
 use frontend\modules\paypal\Paypal;
+use GuzzleHttp\Exception\GuzzleException;
+use Yii;
 use yii\web\Controller;
+use yii\web\Response;
 
 /**
  * Default controller for the `PayPal` module
@@ -12,12 +15,25 @@ class DefaultController extends Controller
 {
     /**
      * Renders the index view for the module
-     * @return string
+     * @return Response|string
+     * @throws GuzzleException
      */
-    public function actionIndex(): string
+    public function actionIndex(): Response
     {
         $paypal = new Paypal();
-        var_dump($paypal->checkoutOrder());
+        $url = $paypal->checkoutOrder();
+        if ($url) {
+            return $this->redirect($url);
+        }
         return $this->render('index');
+    }
+
+    public function actionNotify(): string
+    {
+        $request = Yii::$app->request->post();
+        if (!empty($request)) {
+            Yii::warning($request);
+        }
+        return true;
     }
 }
